a6912f441526c28270a31fb2ea9a1ab961326a5f
freitas-jobson <freitas-jobson@hotmail.com>
2018-12-05 02:28:00 -0200

    Criando repository e service para categoria

---
+++ src/main/java/br/com/unipe/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package br.com.unipe.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.unipe.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository <Categoria, Integer> {
+
+}
---
+++ src/main/java/br/com/unipe/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package br.com.unipe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.unipe.cursomc.domain.Categoria;
+import br.com.unipe.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+}
+
+}
