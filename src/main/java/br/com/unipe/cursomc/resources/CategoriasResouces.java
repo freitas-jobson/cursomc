8276538a29cc6271a45b354658aa3703a2fdbad7
freitas-jobson <freitas-jobson@hotmail.com>
2018-12-18 22:57:59 -0200

    Ajuste no endpoint /categorias/{di}

--- src/main/java/br/com/unipe/cursomc/resources/CategoriasResouces.java
+++ src/main/java/br/com/unipe/cursomc/resources/CategoriasResouces.java
@@ -21,9 +21,12 @@ public class CategoriasResouces {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
-		
+				
 	}
+		
+		
 }
+
